12906e7fe21ba36bc3bc61028d3bbfd6076b244b
Mrityunjay2302 <sonukkc5754@gmail.com>
2023-03-25 11:19:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Context from "./Utils/Context";
+
+function App() {
+  return (
+    <div className="App">
+      <Context />
+    </div>
+  );
+}
+
+export default App;
